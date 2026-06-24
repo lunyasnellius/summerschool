d30f811ef99a23483c8dc80519420133660f9d62
L Meyer LUMI <lmeyer@uni-bonn.de>
2026-06-24 12:06:08 +0300

    wrapping up mpi-02

--- mpi/exercises/02-message-exchange/main-ex.cpp
+++ mpi/exercises/02-message-exchange/main-ex.cpp
@@ -1,4 +1,5 @@
 #include <mpi.h>
+#include <iostream>
 
 int main(int argc, char *argv[]) {
 	MPI_Init(&argc, &argv);
@@ -6,40 +7,41 @@ int main(int argc, char *argv[]) {
 	int rank;
 	int size;
 	MPI_Comm_rank(MPI_COMM_WORLD, &rank);
-	MPI_Comm_size(MPI_COMM_WORLS, &size);
+	MPI_Comm_size(MPI_COMM_WORLD, &size);
 
-	int send-message[size];
-	for(int i=0; i<size; ++i) {
-		message[i] = rank;
+	int msg_size=100000;
+	int send_message[msg_size];
+	for(int i=0; i<msg_size; ++i) {
+		send_message[i] = rank;
 	}
 
-	int to-rank;
-	int from-rank;
+	int to_rank;
+	int from_rank;
 	if(rank == size-1) {
-		to-rank = 0;
-		from-rank = rank-1;
+		to_rank = 0;
+		from_rank = rank-1;
 	} else if(rank == 0) {
-		to-rank = rank+1;
-		from-rank = size-1;
+		to_rank = rank+1;
+		from_rank = size-1;
 	} else {
-		to-rank = rank+1;
-		from-rank = rank-1;
+		to_rank = rank+1;
+		from_rank = rank-1;
 	}
 
-	int recv-message[size];
+	int recv_message[size];
 	if(rank%2 == 0) {
-		MPI_Send(*send-message, size, MPI_INT, to-rank, rank, MPI_COMM_WORLD);
+		MPI_Send(send_message, msg_size, MPI_INT, to_rank, rank, MPI_COMM_WORLD);
 	} else {
-		MPI_Recv(*recv-message, size, MPI_INT, from-rank, rank, MPI_COMM_WORLD);
+		MPI_Recv(recv_message, msg_size, MPI_INT, from_rank, MPI_ANY_TAG, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
 	}
 	if(rank%2 == 0) {
-		MPI_Recv(*recv-message, size, MPI_INT, from-rank, rank, MPI_COMM_WORLD);
+		MPI_Recv(recv_message, msg_size, MPI_INT, from_rank, MPI_ANY_TAG, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
 	} else {
-		MPI_Send(*send-message, size, MPI_INT, to-rank, rank, MPI_COMM_WORLD);
+		MPI_Send(send_message, msg_size, MPI_INT, to_rank, rank, MPI_COMM_WORLD);
 	}
 
 	printf("On rank %i, the sent message is %i and the received message is %i\n",
-			rank, send-message[0], recv-message[0]);
+			rank, send_message[0], recv_message[0]);
 
 	MPI_Finalize();
 }
