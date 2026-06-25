a941e4d4ce4125e09c1620dee6fde128cf4d6807
L Meyer LUMI <lmeyer@uni-bonn.de>
2026-06-25 14:59:28 +0300

    move slurm script everywhere

---
+++ mpi/exercises/10-parallel-pi-reduction/lunas-job.sh
@@ -0,0 +1,13 @@
+#!/bin/bash -x
+#SBATCH --account=project_462001452
+#SBATCH --reservation=SummerSchoolCPU
+#SBATCH --partition=small
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=2
+#SBATCH --time=00:05:00
+#SBATCH --output=out/%x.%j.out
+#SBATCH --error=out/%x.%j.err
+
+# Run the program
+srun $@
+#srun $SLURM_JOB_NAME
